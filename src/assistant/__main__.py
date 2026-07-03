44b1095d1367f3d6189688686aecdd5ac4c1aeba
ClupTheGreat <playclupfree@gmail.com>
2026-07-03 19:15:25 +0000

    Made a uv package to help with build

--- src/assistant/__main__.py
+++ src/assistant/__main__.py
@@ -1,11 +1,15 @@
 from assistant.cli.commands import run
+from pathlib import Path
 import logging
+
+log_dir = Path(__file__).parent / "logs"
+log_dir.mkdir(exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s %(name)s: %(message)s]',
     handlers=[
-        logging.FileHandler('logs/application.log')
+        logging.FileHandler(log_dir / 'application.log')
     ]
 )
 
